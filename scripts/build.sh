10ebab8923635efffe8e922b518d0adc54d7d2ae
David Waring <dev@davidwaring.net>
2020-10-01 16:08:53 -0400

    Fix image tagging

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,8 @@ SCRIPTS_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 DOCKER_DIR="$(dirname $SCRIPTS_DIR)"
 
 # use the yyyymmdd timestamp as the tag
-export T3_BB_TAG=$(date "+%Y%m%d")
+T3_BB_TAG=$(date "+%Y%m%d")
+echo "$T3_BB_TAG" > "$SCRIPTS_DIR/.tag"
 
 # update the repos
 "$SCRIPTS_DIR/prepare.sh"
